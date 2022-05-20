0364325f2f1212757aa118b1d2c0c236120d7cd2
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-05-20 09:37:22 +0100

    Add csv parse pipe

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const fs = require('fs')
 const results = []
 
 fs.createReadStream('kepler_data.csv')
+.pipe(parse({
+    comment: '#',
+    columns: true,
+    
+}))
 .on('data', (data)=>{
     results.push(data)
 }).on('error', (err)=>{
